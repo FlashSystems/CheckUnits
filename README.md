0bfea182c6bebde46ac1b249108c7549ffa40ab9
Daniel Goß <developer@flashsystems.de>
2018-09-03 21:15:09 +0200

    Erster Import.

---
+++ README.md
@@ -0,0 +1,2 @@
+# CheckUnits
+This shell script checks the systemd configuration of a modern Linux based system and makes suggestions to optimize the use of systemd.
---
+++ checkunits.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+colorBold=$(tput smul)
+colorInfo=$(tput setaf black; tput setab 12)
+colorWarn=$(tput setaf black; tput setab 11)
+colorError=$(tput setaf black; tput setab 9)
+colorCode=$(tput bold)
+colorReset=$(tput sgr0)
+
+function CheckState () {
+	[ "$unitType" == "oneshot" ] && return
+	[ "$unitState" == "transient" ] && return
+	
+	local remarks=()
+
+	[ "$activeState" == "failed" ] && remarks+=("E: Unit is is failed state.:Check why it has failed using {{{systemctl status $id}}} or use {{{systemctl reset-failed $id}}} to reset the failed state of the unit.")
+
+	case "$unitState" in
+		enabled)
+			[ "$unitState" == "$preset" ] || remarks+=("I: Unit is enabled but preset wants it to be $preset.:Create a preset file in {{{/etc/systemd/system-preset/}}} containing {{{enable $id}}}")
+
+			# If the unit is enabled it should not be inactive. If it's in failed state we've already reported this.
+			[ "$activeState" == "inactive" ] && remarks+=("W: Unit is enabled but not active.:Use {{{systemctl start $id}}} to start the unit.")
+			;;
+		disabled)
+			[ "$unitState" == "$preset" ] || remarks+=("I: Unit is disabled but preset wants it to be $preset.:Create a preset file in {{{/etc/systemd/system-preset/}}} containing {{{disable $id}}}.")
+
+			# If the unit is disabled it should be inactive as long as it's not triggered by another unit
+			[ "$activeState" == "inactive" ] || [ "$triggeredBy" == "" ] || remarks+=("W: Unit is disabled but $activeState.:Use {{{systemctl stop $id}}} to stop the unit.")
+			;;
+	esac
+
+	if [ ${#remarks[@]} -gt 0 ]; then
+		echo "Remarks for unit $colorBold$id$colorReset:"
+		while IFS=":" read severity msg suggestion; do
+			case "$severity" in
+				I) echo -en "$colorInfo[ INFO  ]" ;;
+				W) echo -en "$colorWarn[WARNING]" ;;
+				E) echo -en "$colorError[ ERROR ]" ;;
+			esac
+			echo -en "$colorReset"
+			echo "$msg"
+			if [ -n "$suggestion" ]; then
+				suggestion=${suggestion//\{\{\{/$colorCode}
+				suggestion=${suggestion//\}\}\}/$colorReset}
+				echo -e "$suggestion$colorReset"
+			fi
+			echo
+		done <<< "$remarks"
+	fi
+}
+
+while IFS="=" read -r key value; do
+	if [ -z "$key" ]; then
+		CheckState
+	else
+		case "$key" in
+			Id) id="$value" ;;
+			Type) unitType="$value" ;;
+			UnitFileState) unitState="$value" ;;
+			ActiveState) activeState="$value" ;;
+			TriggeredBy) triggeredby="$value" ;;
+			UnitFilePreset) preset="$value" ;;
+		esac
+	fi
+done < <(systemctl show --all '*')
+
+CheckState
