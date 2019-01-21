d7b1366a17013be877a56b51c8c783a87fc54cae
Daniel Goß <developer@flashsystems.de>
2019-01-21 20:50:16 +0100

    Bump version number to 0.8

--- checkunits.sh
+++ checkunits.sh
@@ -3,7 +3,7 @@
 # Version and Commit ID
 # shellcheck disable=SC2016
 COMMIT='$Id$'
-VERSION="0.7"
+VERSION="0.8"
 
 # Check the shell version
 if [ -z "${BASH_VERSINFO[*]}" ] || [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
